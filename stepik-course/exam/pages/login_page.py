9bb21ab17cbd2b147984f4aa26e3cab4df68dfda
kadirovgm <kadirovgm@gmail.com>
2021-11-07 19:42:24 +0300

    login page done

--- stepik-course/exam/pages/login_page.py
+++ stepik-course/exam/pages/login_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-
+from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -9,12 +9,15 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert True
+        assert self.browser.current_url in self.url, "Incorrect URL!"
+        # assert self.browser.LoginPageLocators.LOGIN_FORM in self.browser.current_url, "Incorrect URL!"
+
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
--- stepik-course/exam/test_main_page.py
+++ stepik-course/exam/test_main_page.py
@@ -1,6 +1,7 @@
 # pytest -v -s --tb=line --language=en test_main_page.py
 
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 # can go -> main_page
 def test_guest_can_go_to_login_page(browser):
@@ -15,3 +16,10 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+# should be login page -> login_page
+def test_should_be_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
